b0110d0d68933e6a2d92469014eb247a55bf8b8f
yeiandy <74942590+yeiandy@users.noreply.github.com>
2021-05-04 18:00:45 -0500

    Archivos trabajados para trabajo académico
    Archivos trabajados para trabajo académico Front-End

---
+++ archivos-modificados/Css-general.php
@@ -0,0 +1,139 @@
+<?php
+////////////////////////////////////////////////////////////////////////////////
+//Propuesta mejoras
+////////////////////////////////////////////////////////////////////////////////
+// Last modified 05/aug/2012 by cassio@ime.usp.br
+$corfundo = "#758BFD";
+$corfrente = "#000000";
+$corfundo2 = "#dfdfdf";
+$cormenu = "#dfdfdf";
+?>
+
+.faq {
+	margin: auto;
+  	width: 50%;
+  	border: 3px solid #161c79;
+  	padding: 10px;
+}
+a.question
+{
+	text-decoration: none;
+	color: yellow;
+}
+
+
+#menuToggle
+{
+  display: block;
+  position: relative;
+  top: 50px;
+  left: 50px;
+  
+  z-index: 1;
+}
+
+#menuToggle a
+{
+  text-decoration: none;
+  color: #232323;
+  
+  transition: color 0.3s ease;
+}
+
+#menuToggle a:hover
+{
+  color: red;
+}
+
+
+#menuToggle input
+{
+  display: block;
+  width: 40px;
+  height: 32px;
+  position: absolute;
+  top: -7px;
+  left: -5px;
+  
+  cursor: pointer;
+  
+  opacity: 0;
+  z-index: 2; 
+}
+
+#menuToggle span
+{
+  display: block;
+  width: 33px;
+  height: 4px;
+  margin-bottom: 5px;
+  position: relative;
+  
+  background: #cdcdcd;
+  border-radius: 3px;
+  
+  z-index: 1;
+  
+  transform-origin: 4px 0px;
+  
+  transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
+              background 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
+              opacity 0.55s ease;
+}
+
+#menuToggle span:first-child
+{
+  transform-origin: 0% 0%;
+}
+
+#menuToggle span:nth-last-child(2)
+{
+  transform-origin: 0% 100%;
+}
+
+#menuToggle input:checked ~ span
+{
+  opacity: 1;
+  transform: rotate(45deg) translate(-2px, -1px);
+  background: #232323;
+}
+
+#menuToggle input:checked ~ span:nth-last-child(3)
+{
+  opacity: 0;
+  transform: rotate(0deg) scale(0.2, 0.2);
+}
+
+#menuToggle input:checked ~ span:nth-last-child(2)
+{
+  transform: rotate(-45deg) translate(0, -1px);
+}
+
+#menu
+{
+  position: absolute;
+  width: 300px;
+  margin: -68px 0 0 -50px;
+  padding: 50px;
+  padding-top: 125px;
+  
+  background: #ededed;
+  list-style-type: none;
+  
+  transform-origin: 0% 0%;
+  transform: translate(-100%, 0);
+  
+  transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0);
+}
+
+#menu li
+{
+  padding: 10px 0;
+  font-size: 22px;
+}
+
+#menuToggle input:checked ~ ul
+{
+  transform: none;
+}
+
---
+++ archivos-modificados/system/faq.php
@@ -0,0 +1,38 @@
+<?php
+////////////////////////////////////////////////////////////////////////////////
+//Mejoras - FAQ - Preguntas frecuentes
+
+require 'header.php';
+?>
+
+<div class="faq">
+	<ul>
+
+	<h3>Preguntas frequentes</h3>
+
+	<li>
+		<a href="#" class="question">lenguajes permitidos</a>
+		<div class="answer">Java, C++, Node</div>
+	</li>
+
+	<li>
+		<a href="#" class="question">Número de miembros del equipo</a>
+		<div class="answer">Hasta 4 personas por equipo</div>
+	</li>
+
+	<li>
+		<a href="#" class="question">Es posible realizar preguntas durante la prueba</a>
+		<div class="answer">Sí, a través del módulo de Aclaraciones de la prueba.</div>
+	</li>
+	
+	<li>
+		<a href="#" class="question">Criterios para ganar</a>
+		<div class="answer">Código compila, resuelve el problema, se ejecuta en menor tiempo</div>
+	</li>
+
+	</ul>
+</div>
+
+
+</body>
+</html>
